7a20bc9c797cb783a48106305b5e35d5c5517a3a
Paul Hammant <paul@hammant.org>
2018-01-05 09:20:56 -0500

    no need for quotes

--- fixup.sh
+++ fixup.sh
@@ -3,3 +3,4 @@
 # watch for first few lines of file - includes
 
 sed -i '' -E 's/([[:digit:]]+\.[[:digit:]]{1})[[:digit:]]*/\1/g' index.html
+sed -i '' -E 's/&quot;#markerArrowBlock&quot;/#markerArrowBlock/g' index.html
