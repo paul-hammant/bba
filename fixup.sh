3b4098c210decab7a9f80e7835de5f288b9ff3f9
Paul Hammant <paul@hammant.org>
2018-01-05 09:35:32 -0500

    move font definition to start

--- fixup.sh
+++ fixup.sh
@@ -4,3 +4,5 @@
 
 sed -i '' -E 's/([[:digit:]]+\.[[:digit:]]{1})[[:digit:]]*/\1/g' index.html
 sed -i '' -E 's/&quot;#markerArrowBlock&quot;/#markerArrowBlock/g' index.html
+
+sed -i '' -E 's/style="font-size: 16px; font-family: Andale Mono, monospace;"//g' index.html
