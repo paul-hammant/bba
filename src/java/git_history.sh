f4cd2aa0ee05348d84c00b5a04c7eaf9b1db9265
Paul Hammant <paul@hammant.org>
2018-01-19 16:45:42 -0500

    more

--- src/java/git_history.sh
+++ src/java/git_history.sh
@@ -13,35 +13,37 @@ git tag -d 1
 git tag -d 2
 git tag -d 3
 git tag -d 4
+git tag -d 5
 # git add .
 # git commit -m "As Jooby's 'quickstart project generator' made it"
+git tag 1 -m "As Jooby's 'quickstart project generator' made it"
 cd ..
 cp -r 2/trunk/* repo/
 cd repo
 echo "target\nlog/\n*.iml\n" > .gitignore
 git add .
-git commit -m "1. Hair color added that's stringified (regretably in hindsight)"
-git tag 1 -m "Hair color added that's stringified (regretably in hindsight)"
+git commit -m "Hair color added that's stringified (regretably in hindsight)"
+git tag 2 -m "Hair color added that's stringified (regretably in hindsight)"
 cd ..
 cp -r 3/trunk/* repo/
 cd repo
 git add .
-git commit -m "2. Extract hard-coded string version of hair color to a factory - introducing the abstraction (and go live)"
-git tag 2 -m "Extract hard-coded string version of hair color to a factory - introducing the abstraction (and go live)"
+git commit -m "Extract hard-coded string version of hair color to a factory - introducing the abstraction (and go live)"
+git tag 3 -m "Extract hard-coded string version of hair color to a factory - introducing the abstraction (and go live)"
 cd ..
 cp -r 4/trunk/* repo/
 cd repo
 git add .
-git commit -m "3. New second enum-based impl of 'hair color' (with tests) without deleting the old impl (and go live)"
-git tag 3 -m "New second enum-based impl of 'hair color' (with tests) without deleting the old impl (and go live)"
+git commit -m "New second enum-based impl of 'hair color' (with tests) without deleting the old impl (and go live)"
+git tag 4 -m "New second enum-based impl of 'hair color' (with tests) without deleting the old impl (and go live)"
 cd ..
 cp -r 8/trunk/* repo/
 cd repo
 git add .
 git rm conf/application_release4.conf
 git rm src/main/java/com/mycompany/Release3.java
-git commit -m "5. Remove the hair color abstraction (some time after the release as we're passed roll back now, and go live)"
-git tag 4 -m "Remove the hair color abstraction (some time after the release as we're passed roll back now, and go live)"
+git commit -m "Remove the hair color abstraction (some time after the release as we're passed roll back now, and go live)"
+git tag 5 -m "Remove the hair color abstraction (some time after the release as we're passed roll back now, and go live)"
 git push -f
 git push --tags -f
 cd ..
