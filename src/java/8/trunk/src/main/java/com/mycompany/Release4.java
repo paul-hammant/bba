5872eda1645f26388797216b3d339438dc224183
Paul Hammant <paul@hammant.org>
2018-01-20 17:18:38 -0500

    more

--- src/java/8/trunk/src/main/java/com/mycompany/Release4.java
+++ src/java/8/trunk/src/main/java/com/mycompany/Release4.java
@@ -4,7 +4,7 @@ package com.mycompany;
  * @author Paul Hammant DevOps, (c) 2018
  */
 public class Release4 implements ReleaseToggles {
-  // nothing scheduled in the wider release 4 time frame that
+  // nothing scheduled in the release 4 time frame that
   // would require a Branch By Abstraction procedure to
-  // ease it into production
+  // allow it to be coded over a perid of time
 }
